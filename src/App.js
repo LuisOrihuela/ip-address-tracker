63fca99715acbe3644628b5569f228723ffb50d8
Luis Orihuela <luisorihuela@pop-os.localdomain>
2021-10-09 14:55:15 -0500

    Deleted unsued component in App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import styled, { ThemeProvider } from "styled-components";
+import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
-
-const StyledText = styled.div`
-  font-size: ${({ theme }) => theme.fonts.size.main};
-`;
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <StyledText>Home</StyledText>
+      <div>Home</div>
     </ThemeProvider>
   );
 }
